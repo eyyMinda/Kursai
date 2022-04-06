2d3dd1a2ce47c76644406445e7eef9b3bae47e47
eyyMinda <gamerx14x@gmail.com>
2022-04-06 13:25:25 +0300

    got no time to fix

--- src/Designs/Luxestate/js/searchBar.js
+++ src/Designs/Luxestate/js/searchBar.js
@@ -1,5 +1,5 @@
 // $(function () {
-//     $("#includedContent").load("b.html");
+//     $("#includedContent").load("index.html");
 // });
 
 
@@ -43,10 +43,40 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $("#locationCards2").hide();
+    $("#search2").focus(function () {
+        $('#locationCards2').show('fast');
+    });
+    $('#search2').blur(function () {
+        if (!$(this).val()) {
+            $('#locationCards2').hide('fast');
+        }
+    });
+});
+
 function search_location() {
     let input = document.getElementById('search').value
     input = input.toLowerCase();
     let x = document.getElementsByClassName('search_locationCard');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+            console.log("not displayed");
+            $("p").text("No apartments in this location...")
+        }
+        else {
+            x[i].style.display = "block";
+            console.log("displayed");
+            $("p").text("")
+        }
+    }
+}
+function search_location2() {
+    let input = document.getElementById('search2').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('search_locationCard2');
 
     for (i = 0; i < x.length; i++) {
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
