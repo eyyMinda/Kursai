b2e0bba0466b162f87e1d3e82196402639a9adfc
eyyMinda <gamerx14x@gmail.com>
2022-05-05 03:49:03 +0300

    left off at inputFilter

--- src/JavaScript/FakeBook/main.js
+++ src/JavaScript/FakeBook/main.js
@@ -1,61 +1,116 @@
+const myBox = document.getElementById('myBox');
 const Box = document.getElementById('Box');
-// const activityBox = document.getElementById('activityBox');
+const expand = document.getElementById('expand');
 
 const usersUrl = 'https://randomuser.me/api/';
 const activityUrl = 'http://www.boredapi.com/api/activity/';
-const users100 = '?results=100';
+const users100 = '?results=21';
 let users;
+let me;
+
+//search
+
+const input = document.getElementById('searchInput')
+
+//create an activity room
+let joinInterval;
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const paramType = urlParams.get('type')
+let validTypes = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+let roomType;
+if (paramType) {
+    if (validTypes.includes(paramType))
+        roomType = "https://www.boredapi.com/api/activity?type=" + paramType
+}
 
 function getUsers(type) {
     fetch(usersUrl + users100)
         .then(response => response.json())
         .then(data => {
             users = data.results;
-            // keys = Object.keys(users[0])
-            // console.log(users, keys, users[0].picture)
-            getActivity(users, type)
+            me = data.results[20]
+            renderMe(me)
+
+            if (type == 'users') {
+                renderUsers(users)
+                console.log('userss')
+            } else if (type == 'feed') {
+                renderFeed(users)
+                console.log('feeed')
+            }
+            return users;
         });
 }
 
 getUsers('users')
 
-function getActivity(check, type) {
-    if (type == 'feed') {
-        for (let i = 0; i < 100; i++) {
-            fetch(activityUrl)
-                .then(response => response.json())
-                .then(data => {
-                    const activityBox = document.createElement('div')
-                    activityBox.innerHTML += `<h4>${data.activity}</h4><p>Type: ${data.type}, Participants: ${data.participants}`
-                    console.log(activityBox)
-                    renderBox(check, type, activityBox)
-                })
-        }
-    } else {
-        fetch(activityUrl)
-            .then(response => response.json())
-            .then(data => {
-                renderBox(check, type, data)
-            })
-    }
+input.addEventListener('keyup', function() {
+    //blablabla einu miegot its 4am
+})
+
+
+function roomCreation() {
+    expand.innerHTML = ''
+    expand.innerHTML += `
+    <form action="./index.html" method="GET">
+        <label for="activity-select">Choose a type of activity:</label>
+         <select class="bg-dark m-2" name="type" id="activity-select">
+            <option value="">--Please choose an option--</option>
+            <option value="education">Education</option>
+            <option value="recreational">Recreational</option>
+            <option value="social">Social</option>
+            <option value="diy">Diy</option>
+            <option value="charity">Charity</option>
+            <option value="cooking">Cooking</option>
+            <option value="relaxation">Relaxation</option>
+            <option value="music">Music</option>
+            <option value="busywork">Busywork</option>
+         </select>
+        <button type="submit" class="btn bg-info">Create</button>
+    </form>`
 }
 
-function renderBox(array, boxtype, activity) {
-    if (boxtype == 'users') {
-        renderUsers(array)
-        console.log('userss')
-    } else if (boxtype == 'feed') {
-        renderFeed(array, activity)
-        console.log('feeed')
+function renderMe(me) {
+    myBox.innerHTML = ''
+    myBox.innerHTML += `<div class="col border"><img src="${me.picture.large}"><h3>${me.name.first} ${me.name.last}</div>`
+    ///RENDER MY ROOM
+    if (roomType) {
+        expand.style.display = 'none'
+        console.log('roomType')
+        fetch(roomType).then(response => response.json()).then((data) => {
+            myBox.innerHTML += `<ul class="border list-group">
+                <li class="list-group-item bg-dark text-primary">Activity: <b>${data.activity}</b></li><hr>
+                <li class="list-group-item bg-dark text-primary">Activity type: <b>${data.type}</b></li>
+                <li class="list-group-item bg-dark text-primary">Participants required: <b>${data.participants}</b></li>
+                <li class="list-group-item bg-dark text-primary" id="usersJoined">Participants joined: <hr></li>
+                `
+                usersJoined = document.getElementById('usersJoined')
+                let i = 0;
+                joinInterval = setInterval(() => {
+                    const participant = users[Math.floor(Math.random() * 100)]
+        
+                    usersJoined.innerHTML += `<span>
+                    <img src="${participant.picture.thumbnail}">
+                    ${participant.name.first} ${participant.name.last}
+                    </span><br>`
+                    i++
+                    if (i == 4) clearInterval(joinInterval)
+                }, 2000)
+        })
     }
+    myBox.append(expand)
 
 }
+
 function renderUsers(array) {
     Box.textContent = ''
     array.map((user) => {
         const userInfo = document.createElement('div')
         userInfo.classList.add('border')
+        userInfo.classList.add('border-secondary')
         userInfo.classList.add('p-2')
+        userInfo.classList.add('col-3')
 
         const image = document.createElement('img')
         image.src = user.picture.large;
@@ -67,7 +122,7 @@ function renderUsers(array) {
         age.textContent = user.dob.age
 
         const location = document.createElement('p')
-        location.textContent = `${user.location.iry}, ${user.location.city}`
+        location.textContent = `${user.location.country}, ${user.location.city}`
 
         userInfo.appendChild(image);
         userInfo.appendChild(Name);
@@ -76,13 +131,14 @@ function renderUsers(array) {
         Box.appendChild(userInfo)
     })
 }
-function renderFeed(array, info) {
+function renderFeed(array) {
     Box.textContent = ''
     array.map((user) => {
 
         const Userinfo = document.createElement('div')
         Userinfo.classList.add('row')
         Userinfo.classList.add('border')
+        Userinfo.classList.add('border-secondary')
 
         const image = document.createElement('img')
         image.src = user.picture.large;
@@ -92,14 +148,13 @@ function renderFeed(array, info) {
         Name.textContent = `${user.name.first} ${user.name.last}`
         Name.classList.add('col-2')
 
-        // const Activity = document.createElement('div')
-        // Activity.classList.add('col')
-        // Activity.textContent = info[0].activity
-        // Activity.innerHTML += `<h4>info.activity</h4><p>Type: ${info[0].type}, Participants: ${info[0].participants}`
-
         Userinfo.appendChild(image)
         Userinfo.appendChild(Name)
-        Userinfo.appendChild(info)
+        fetch(activityUrl)
+            .then(response => response.json())
+            .then(data => {
+                Userinfo.innerHTML += `<h5 class="col-5 p-4">${data.activity}</h5><div class="col p-2"><p>required participants: <b>${data.participants}</b></p>type of activity: <b>${data.type}</div>`
+            })
 
         Box.appendChild(Userinfo)
     })
--- src/JavaScript/param/main.js
+++ src/JavaScript/param/main.js
@@ -7,14 +7,12 @@ const paramQuantity = urlParams.get('quantity')
 
 let validTypes = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
 
-if(paramType)
-{
-    if(validTypes.includes(paramType))
+if (paramType) {
+    if (validTypes.includes(paramType))
         boredUrl = "https://www.boredapi.com/api/activity?type=" + paramType
 }
 
-for(let count = 0; count < paramQuantity; count++)
-{
+for (let count = 0; count < paramQuantity; count++) {
     fetch(boredUrl).then(response => response.json()
-    .then(activityObject => document.getElementById("activities").innerHTML += activityObject.activity + "." + "<br>"))
+        .then(activityObject => document.getElementById("activities").innerHTML += activityObject.activity + "." + "<br>"))
 }
