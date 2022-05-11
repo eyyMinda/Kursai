d499e654ba757a71be70e487c352afb67e5a0872
eyyMinda <gamerx14x@gmail.com>
2022-05-11 11:08:35 +0300

    tictactoe

--- src/Games/TicTacToe/main.js
+++ src/Games/TicTacToe/main.js
@@ -1,39 +1,91 @@
-const tiles = Array.from(document.querySelectorAll('.tile'));
-const playerDisplay = document.querySelector('.display-player');
-const resetButton = document.getElementById('reset');
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const display = document.querySelector('.display');
+    const resetButton = document.getElementById('reset');
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-let resetBoard;
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+    const XWon = 'Xwon'
+    const OWon = 'Owon'
+    const tie = 'tie'
+    let gameWon;
+    let isGameActive = true
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
 
-let board = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
-
-function userChoice(tile, i) {
-    console.log(`clicked on ${i + 1}`);
-    if (!tile.innerHTML) {
-        if (currentPlayer == 'X') {
-            board[i] = currentPlayer + i
-            tile.innerHTML = `<i class="fa-solid fa-x"></i>`
-            currentPlayer = 'O'
-            playerDisplay.textContent = 'O'
-        } else {
-            board[i] = currentPlayer + i
-            tile.innerHTML = `<i class="fa-solid fa-circle-o">O</i>`
-            currentPlayer = 'X'
-            playerDisplay.textContent = 'X'
-        }
-        console.log(board)
+    function changePlayer() {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDisplay.innerHTML = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
     }
-}
-tiles.forEach((tile, i) => {
-    tile.addEventListener(('click'), () => userChoice(tile, i))
-})
+
+    function checkTile(tile) {
+        if (tile.innerText === 'X' || tile.innerText === 'O') { return false; } return true;
+    };
+
+    function userChoice(tile, i) {
+        if (checkTile(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+            board[i] = currentPlayer;
+            console.log(board)
+            checkResults();
+            changePlayer();
+        }
+    }
+
+    function checkResults() {
+        gameWon = false;
+        for (let i = 0; i <= 7; i++) { //map all conditions
+            const winCondition = winningConditions[i]; //single condition that extends to 3 parts:
+            const a = board[winCondition[0]]; 
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') { continue; } //continue is needed for if you want to check more than one tile
+            if (a === b && b === c) { gameWon = true; break; }
+        }
+        if (gameWon) { endResult(currentPlayer === 'X' ? XWon : OWon); isGameActive = false; return; }
+        if (!board.includes('')) endResult(tie)
+    }
+
+    function endResult(end) {
+        switch (end) {
+            case XWon:
+                display.innerHTML = `<span class="playerX">X</span> Won the Game!`
+                break;
+            case OWon:
+                display.innerHTML = `<span class="playerO">O</span> Won the Game!`
+                break;
+            case tie:
+                display.innerHTML = `It's a tie!`
+                break;
+        }
+    }
+
+    function resetBoard() {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = true;
+        display.innerHTML = `Player <span class="display-player playerX">X</span>'s turn`
+        if (currentPlayer === 'O') changePlayer()
+        tiles.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove(`playerX`);
+            tile.classList.remove(`playerO`);
+        })
+    }
+
+    tiles.forEach((tile, i) => {
+        tile.addEventListener(('click'), () => userChoice(tile, i))
+    })
+    resetButton.addEventListener('click', resetBoard);
+});
